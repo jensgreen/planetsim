c11ed3f3dc9fbacf173c72dfe90b50d2f0756662
Jens Green Olander <jens@apels.in>
2014-05-16 12:08:10 +0200

    Remove unused files & move models.

--- planetsim.c
+++ planetsim.c
@@ -65,12 +65,12 @@ void init(void)
 
   // Load models
   printf("Loading models\n");
-  initSphere(&planets[0],0, 0 ,5000,0, 0,"HD_SPHERE_2015.obj");
+  initSphere(&planets[0],0, 0 ,5000,0, 0,"models/HD_SPHERE_2015.obj");
   scaleSphere(&planets[0],1000);
   initHalo(&haloes[0], -10,-10,5000, "models/billboard.obj");
   scaleHalo(&haloes[0],1150);
 
-  initSphere(&planets[1],0, 0, 10000,0,0,"HD_SPHERE_2015.obj");
+  initSphere(&planets[1],0, 0, 10000,0,0,"models/HD_SPHERE_2015.obj");
   scaleSphere(&planets[1],1000);
 
   // Load terrain data
